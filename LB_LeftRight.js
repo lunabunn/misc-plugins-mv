dfbf97328ca96e7c040b8123106ec3615447b901
lunabunn <iamrabbitmoon@gmail.com>
2021-04-14 18:27:11 -0400

    LeftRight: Add En/DisablePlayerLeftRight

--- LB_LeftRight.js
+++ LB_LeftRight.js
@@ -12,6 +12,10 @@
  * the "Enable for events" plugin parameter. Further, any value other than true
  * (active) and false (deactivate) are ignored.
  * 
+ * Whether or not to fix the direction of player sprites can also be
+ * specified using the EnablePlayerLeftRight / DisablePlayerLeftRight
+ * plugin commands.
+ * 
  * @param players
  * @text Enable for players
  * @desc Enabled/Disables fixing the direction of player sprites to the horizontal axis.
@@ -35,6 +39,9 @@
  * 파라미터의 값과 관계 없이 적용됩니다. 또한, true (활성화) 와
  * false (비활성화) 이외의 값은 무시됩니다.
  * 
+ * EnablePlayerLeftRight / DisablePlayerLeftRight 플러그인 커맨드를 사용해
+ * 플레이어의 고정 여부 역시 설정하실 수 있습니다.
+ * 
  * @param players
  * @text 플레이어에게 사용
  * @desc 스프라이트 고정 기능을 플레이어에게 사용할지 여부
@@ -47,10 +54,16 @@
 {
     const params = PluginManager.parameters("LB_LeftRight");
 
-    const initialize = Sprite_Character.prototype.initialize;
+    const initialize1 = Sprite_Character.prototype.initialize;
     Sprite_Character.prototype.initialize = function(character) {
-        initialize.call(this, character);
+        initialize1.call(this, character);
         this._direction = 6;
+    };
+
+    const initialize2 = Game_Player.prototype.initialize;
+    Game_Player.prototype.initialize = function() {
+        initialize2.call(this);
+        this._leftRight = params["players"] == "true";
     };
 
     const regTrue = /<\s*leftright\s*:\s*true\s*>/;
@@ -58,26 +71,40 @@
     const setCharacter = Sprite_Character.prototype.setCharacter;
     Sprite_Character.prototype.setCharacter = function (character) {
         setCharacter.call(this, character);
-        if (character instanceof Game_Player) {
-            this.leftRight = params["players"] == "true";
-        } else if (character instanceof Game_Event) {
+        if (character instanceof Game_Event) {
             let note = character.event().note;
             if (note) {
                 if (regTrue.test(note)) {
-                    this.leftRight = true;
+                    character._leftRight = true;
                 } else if (regFalse.test(note)) {
-                    this.leftRight = false;
+                    character._leftRight = false;
                 } else {
-                    this.leftRight = params["events"] == "true";
+                    character._leftRight = params["events"] == "true";
                 }
             } else {
-                this.leftRight = params["events"] == "true";
+                character._leftRight = params["events"] == "true";
             }
         }
     };
 
+    const pluginCommand = Game_Interpreter.prototype.pluginCommand;
+    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+        pluginCommand.call(this, command, args);
+        if (command == "EnablePlayerLeftRight") {
+            $gamePlayer._leftRight = true;
+            $gamePlayer._followers.forEach(follower => {
+                follower._leftRight = true;
+            });
+        } else if (command == "DisablePlayerLeftRight") {
+            $gamePlayer._leftRight = false;
+            $gamePlayer._followers.forEach(follower => {
+                follower._leftRight = false;
+            });
+        }
+    };
+
     Sprite_Character.prototype.characterPatternY = function () {
-        if (!this.leftRight) {
+        if (!this._character._leftRight) {
             return (this._character._direction - 2) / 2;
         }
         if (this._character._direction == 4 || this._character._direction == 6) {
