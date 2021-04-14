e93a9cc0131006b34719e28c14d6ab9edf82696a
lunabunn <iamrabbitmoon@gmail.com>
2021-04-14 18:41:29 -0400

    LeftRight: Add license (zlib)

--- LB_LeftRight.js
+++ LB_LeftRight.js
@@ -2,6 +2,26 @@
 // LB_LeftRight.js
 //=============================================================================
 
+/**
+ * Copyright (c) 2021 lunabunn
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
 /*:
  * @plugindesc Fixes the diection of character sprites to the horizontal axis (left and right).
  * @author LunaBunn
@@ -55,13 +75,13 @@
     const params = PluginManager.parameters("LB_LeftRight");
 
     const initialize1 = Sprite_Character.prototype.initialize;
-    Sprite_Character.prototype.initialize = function(character) {
+    Sprite_Character.prototype.initialize = function (character) {
         initialize1.call(this, character);
         this._direction = 6;
     };
 
     const initialize2 = Game_Player.prototype.initialize;
-    Game_Player.prototype.initialize = function() {
+    Game_Player.prototype.initialize = function () {
         initialize2.call(this);
         this._leftRight = params["players"] == "true";
     };
@@ -88,7 +108,7 @@
     };
 
     const pluginCommand = Game_Interpreter.prototype.pluginCommand;
-    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    Game_Interpreter.prototype.pluginCommand = function (command, args) {
         pluginCommand.call(this, command, args);
         if (command == "EnablePlayerLeftRight") {
             $gamePlayer._leftRight = true;
