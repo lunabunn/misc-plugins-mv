dbeb8753e6fbd62c93c6ca1425725a31205027de
lunabunn <iamrabbitmoon@gmail.com>
2021-06-30 20:31:07 -0400

    JumpHeight: Add jump speed control

--- LB_JumpHeight.js
+++ LB_JumpHeight.js
@@ -23,11 +23,20 @@
  */
 
 /*:
- * @plugindesc 점프 높이를 조정할 수 있는 플러그인입니다.
+ * @plugindesc Lets you control the height and speed of jumps.
+ * @author LunaBunn
+ *
+ * @help This plugin can be used by adding a script command
+ * to your Set Movement Route event and calling the following:
+ * this.jump(X offset, Y offset, Jump height (Default: 1), Jump speed (Default: 1));
+ */
+
+/*:ko
+ * @plugindesc 점프 높이와 속도를 조정할 수 있는 플러그인입니다.
  * @author LunaBunn
  *
  * @help 이동 루트 설정 이벤트 내부에서 스크립트 명령을 추가하신 뒤
- * this.jump(X 오프셋, Y 오프셋, 점프 높이 (1이 기본));
+ * this.jump(X 오프셋, Y 오프셋, 점프 높이 (1이 기본), 점프 속도 (1이 기본));
  * 을 호출하는 것으로 사용하실 수 있습니다.
  */
 
@@ -36,6 +45,7 @@
     Game_CharacterBase.prototype.initMembers = function() {
         initMembers.call(this);
         this._jumpHeightFactor = 1;
+        this._jumpSpeedFactor = 1;
     }
 
     const jumpHeight = Game_CharacterBase.prototype.jumpHeight;
@@ -43,14 +53,21 @@
         return jumpHeight.call(this) * this._jumpHeightFactor;
     };
 
+    const updateJump = Game_CharacterBase.prototype.updateJump;
+    Game_CharacterBase.prototype.updateJump = function() {
+        this._jumpCount += 1 - this._jumpSpeedFactor;
+        updateJump.call(this);
+    };
+
     const jump = Game_CharacterBase.prototype.jump;
-    Game_CharacterBase.prototype.jump = function(xPlus, yPlus, factor=1) {
-        this._jumpHeightFactor = factor;
+    Game_CharacterBase.prototype.jump = function(xPlus, yPlus, heightFactor=1, speedFactor=1) {
+        this._jumpHeightFactor = heightFactor;
+        this._jumpSpeedFactor = speedFactor;
         jump.call(this, xPlus, yPlus);
     };
 
-    Game_Player.prototype.jump = function(xPlus, yPlus, factor) {
-        Game_Character.prototype.jump.call(this, xPlus, yPlus, factor);
+    Game_Player.prototype.jump = function(xPlus, yPlus, heightFactor, speedFactor) {
+        Game_Character.prototype.jump.call(this, xPlus, yPlus, heightFactor, speedFactor);
         this._followers.jumpAll();
     };
 
@@ -60,8 +77,9 @@
                 var follower = this._data[i];
                 var sx = $gamePlayer.deltaXFrom(follower.x);
                 var sy = $gamePlayer.deltaYFrom(follower.y);
-                var factor = $gamePlayer._jumpHeightFactor;
-                follower.jump(sx, sy, factor);
+                var heightFactor = $gamePlayer._jumpHeightFactor;
+                var speedFactor = $gamePlayer._jumpSpeedFactor;
+                follower.jump(sx, sy, heightFactor, speedFactor);
             }
         }
     };
